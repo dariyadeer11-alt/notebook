609da82ddd4465ceafd6c6ca9191816136321f4a
Darya <driyadeer11@gmail.com>
2025-11-27 22:30:08 +0300

    Добавила содержимое в storage

---
+++ storage.py
@@ -0,0 +1,20 @@
+# notebookk/storage.py
+import json
+import os
+from .models import Note
+
+FILE_PATH = "notes.json"
+
+def load_notes():
+    if not os.path.exists(FILE_PATH):
+        return []
+    try:
+        with open(FILE_PATH, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            return [Note.from_dict(item) for item in data]
+    except Exception:
+        return []
+
+def save_notes(notes):
+    with open(FILE_PATH, "w", encoding="utf-8") as f:
+        json.dump([n.to_dict() for n in notes], f, indent=4, ensure_ascii=False)
